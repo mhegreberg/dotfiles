1f98ad538117a19bb949e8f5ad4a15e0d9a76bca
Mark Hegreberg <mark@hegreberg.io>
2021-08-31 20:31:41 -0700

    [SWAY] show connected network on swaybar

--- sway/.config/sway/scripts/sway_bar.sh
+++ sway/.config/sway/scripts/sway_bar.sh
@@ -26,8 +26,10 @@ audio_is_muted=$(pamixer --sink `pactl list sinks short | grep RUNNING | awk '{p
 # Network
 network=$(ip route get 1.1.1.1 | grep -Po '(?<=dev\s)\w+' | cut -f1 -d ' ')
 # interface_easyname grabs the "old" interface name before systemd renamed it
-interface_easyname=$(dmesg | grep $network | grep renamed | awk 'NF>1{print $NF}')
+interface_easyname=$(ip route get 1.1.1.1 | grep -Pa '(?<=dev\s)\w+' | awk '{print $5}')
 ping=$(ping -c 1 www.google.com | tail -1| awk '{print $4}' | cut -d '/' -f 2 | cut -d '.' -f 1)
+netname=$(iwctl station wlan0 show | grep 'Connected network' | awk '{$1=$2=""; print $0}')
+
 
 
 # Others
@@ -51,7 +53,7 @@ if ! [ $network ]
 then
    network_active="⛔"
 else
-   network_active="⇆"
+   network_active=$netname"  ⇆ "
 fi
 
 if [ $audio_volume = "0%" ]
